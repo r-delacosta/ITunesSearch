5a8fac67a8f5cc93ad7309cc08924c22acb69961
r-delacosta <59967513+r-delacosta@users.noreply.github.com>
2021-01-20 11:45:44 +0800

    ui 2

--- app/src/main/java/com/rdelacosta/itunessearch/ui/musicDetail/DetailFragment.kt
+++ app/src/main/java/com/rdelacosta/itunessearch/ui/musicDetail/DetailFragment.kt
@@ -31,39 +31,36 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
             textTrack.text = viewModel.music?.trackName
             textArtist.text = viewModel.music?.artistName
             textCollection.text = viewModel.music?.collectionName
-            textTime.text = viewModel.music?.trackTimeMillis?.let { TimeFormatter.toMinuteSecondFormat(it) }
-            textPrice.text = viewModel.music?.currency + viewModel.music?.trackPrice
-            textReleaseDate.text = viewModel.music?.releaseDate?.let { TimeFormatter.toDateFormat(it) }
-            textGenre.text = viewModel.music?.primaryGenreName
-            textExplicitness.text = viewModel.music?.trackExplicitness
-            Picasso.get().load(viewModel.music?.artworkUrl100)
+            textTime.text = "Duration: " + viewModel.music?.trackTimeMillis?.let { TimeFormatter.toMinuteSecondFormat(it) }
+            textPrice.text = "Price: " + viewModel.music?.currency + viewModel.music?.trackPrice
+            textReleaseDate.text = "Release Date: " +  viewModel.music?.releaseDate?.let { TimeFormatter.toDateFormat(it) }
+            textGenre.text = "Genre: " + viewModel.music?.primaryGenreName
+            if (viewModel.music?.trackExplicitness.equals("explicit"))
+                textExplicitness.text = "Explicit"
+            else
+                textExplicitness.text = "Not Explicit"
+
+            Picasso.get().load((viewModel.music?.artworkUrl100)?.replace("100x100", "1000x1000"))
                 .placeholder(R.drawable.progress_animation)
                 .error(R.drawable.ic_baseline_error_24)
-                .resize(200,200)
                 .into(imageView)
         }
     }
 
     private fun setupListeners() {
-        binding.textArtist.setOnClickListener() {
+        binding.textArtist.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(viewModel.music?.artistViewUrl)
             startActivity(intent)
         }
 
-        binding.textArtist.setOnClickListener() {
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse(viewModel.music?.artistViewUrl)
-            startActivity(intent)
-        }
-
-        binding.textTrack.setOnClickListener() {
+        binding.textTrack.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(viewModel.music?.trackViewUrl)
             startActivity(intent)
         }
 
-        binding.textCollection.setOnClickListener() {
+        binding.textCollection.setOnClickListener {
             val intent = Intent(Intent.ACTION_VIEW)
             intent.data = Uri.parse(viewModel.music?.collectionViewUrl)
             startActivity(intent)
